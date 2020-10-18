5c8406f971412fd3198e4d25ee296e94d379ae93
Gerard Olenski <gerard.olenski@gmail.com>
2020-10-18 11:23:36 +0200

    Slight fixes

--- src/test/java/org/gol/taskmanager/TaskManagerApplicationTests.java
+++ src/test/java/org/gol/taskmanager/TaskManagerApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class WorkerManagerPortApplicationTests {
+class TaskManagerApplicationTests {
 
     @Test
     void contextLoads() {
