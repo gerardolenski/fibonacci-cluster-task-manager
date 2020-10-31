f6e3d99c20ffbedf0d13f9ea36ecdb506589a1f9
Gerard Olenski <gerard.olenski@gmail.com>
2020-10-31 07:17:19 +0100

    Improve AMQ producer configuration

---
+++ src/main/java/org/gol/taskmanager/infrastructure/amq/AmqProducerConfig.java
@@ -0,0 +1,49 @@
+package org.gol.taskmanager.infrastructure.amq;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import org.gol.taskmanager.domain.config.ConfigurationPort;
+import org.gol.taskmanager.domain.manager.WorkerNotificationPort;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+
+import javax.jms.ConnectionFactory;
+
+import static com.fasterxml.jackson.annotation.JsonCreator.Mode.PROPERTIES;
+import static org.springframework.jms.support.converter.MessageType.TEXT;
+
+@Configuration
+class AmqProducerConfig {
+
+    private static final String TYPE_ID_PROPERTY_NAME = "_type";
+
+    @Bean
+    WorkerNotificationPort amqWorkerManagerAdapter(
+            @Qualifier("jmsConnectionFactory") ConnectionFactory connectionFactory,
+            ConfigurationPort configurationPort) {
+        return new AmqWorkerManagerAdapter(initJmsTemplate(connectionFactory), configurationPort.getWorkerQueueName());
+    }
+
+    private JmsTemplate initJmsTemplate(ConnectionFactory connectionFactory) {
+        var jmsTemplate = new JmsTemplate(connectionFactory);
+        jmsTemplate.setMessageConverter(initJmsMessageConverter());
+        return jmsTemplate;
+    }
+
+    private MessageConverter initJmsMessageConverter() {
+        var converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(TEXT);
+        converter.setTypeIdPropertyName(TYPE_ID_PROPERTY_NAME);
+        converter.setObjectMapper(initJmsObjectMapper());
+        return converter;
+    }
+
+    private ObjectMapper initJmsObjectMapper() {
+        return new ObjectMapper()
+                .registerModule(new ParameterNamesModule(PROPERTIES));
+    }
+}
